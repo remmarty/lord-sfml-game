33ff6564bac334746d52f56ae1b74331bd99ce4d
Eqebenezer <remi.martyniak@linux.pl>
2017-06-05 18:36:45 +0200

    Add tmx parser, make it compile
    TODO display map, and wrap it into methods

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
 #include <SFML/Graphics.hpp>
 #include "classes/engine/GameEngine.h"
+#include "tmx_parser/tmxparser.h"
 
 int main() {
+    // testing if parser works and gives no errors
+    tmxparser::TmxMap map;
+    tmxparser::TmxReturn error = tmxparser::parseFromFile("../maps/test_map.tmx", &map, "../tilesets/terrain.png");
 
     GameEngine engine;
     engine.run();
